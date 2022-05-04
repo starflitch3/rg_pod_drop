85121bd69b80042d9364d6e7b3a652bf706368d2
tbrankov <trbr60645@eleve.isep.fr>
2022-05-04 16:54:05 +0200

    Afficher héros dans l'équipe dans main-view

--- src/main/java/isep/jfx/MainController.java
+++ src/main/java/isep/jfx/MainController.java
@@ -1,13 +1,27 @@
 package isep.jfx;
 
+import isep.rpg.Game;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 import java.io.IOException;
 
 public class MainController {
+
+    @FXML
+    ListView<String> teamList;
+    @FXML
+    Button addHealer;
+    @FXML
+    Button addMage;
+    @FXML
+    Button addWarrior;
+    @FXML
+    Button addHunter;
 
     @FXML
     protected void onStartButtonClick() throws IOException {
@@ -17,6 +31,16 @@ public class MainController {
         Scene scene = new Scene(fxmlLoader.load());
         MainApplication.stage.setScene(scene);
         MainApplication.stage.show();
+    }
+
+    @FXML
+    public void initialize() {
+        Game.playGame();
+        updateTeamListView();
+    }
+
+    private void updateTeamListView() {
+        teamList.getItems().setAll(Game.context.getHeroesStatus());
     }
 
 }
